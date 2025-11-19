e130afd98580cf47dc5a1aa5bc13a678a7c1f338
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-19 14:17:32 +0530

    oop practice project

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,18 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    public void displayFName() {
+        System.out.println("First Name is:"+firstName);
+        System.out.println("Last Name is:"+ lastName);
+    }
+    public void displayAge() {
+        System.out.println("Age is:"+ age);
+    }
 }
