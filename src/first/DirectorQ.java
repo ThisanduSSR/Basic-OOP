8aeead3b5beee7b6a5db933ba2ddd28b96b56c44
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-20 08:54:10 +0530

    oop practice project

--- src/first/DirectorQ.java
+++ src/first/DirectorQ.java
@@ -1,4 +1,36 @@
 package first;
 
 public class DirectorQ {
+    private String name;
+    private String surname;
+    private int numDirectedMovie;
+    private Date dob;
+
+    public DirectorQ(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+    public void setNumDirectedMovie(int numDirectedMovie) {
+        this.numDirectedMovie = numDirectedMovie;
+    }
+    public int getNumDirectedMovie() {
+        return numDirectedMovie;
+    }
+    public Date getDob(){
+        return dob;
+    }
+    @Override
+    public String toString() {
+        return name+ " "+ surname+" "+numDirectedMovie+" "+dob;
+    }
 }
+
