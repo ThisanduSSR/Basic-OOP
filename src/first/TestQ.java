10f9581a55b4035d26b69e2d10acc789594a06f5
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-20 09:32:08 +0530

    oop practice project

--- src/first/TestQ.java
+++ src/first/TestQ.java
@@ -1,4 +1,13 @@
 package first;
 
 public class TestQ {
+    public static void main(String[] args){
+        DateQ dob = new DateQ(16,8,2008);
+        DirectorQ director = new DirectorQ("James", "Cameron");
+        director.setDob(dob);
+        director.setNumDirectedMovie(23);
+
+        System.out.println(director);
+    }
+
 }
