9337fd92e48a7590ab9830e85475c36165753ace
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-22 02:42:12 +0530

    oop practice project

---
+++ src/TestCylinder.java
@@ -0,0 +1,12 @@
+public class TestCylinder {
+    public static void main(String[] args){
+
+        Cylinder4 c = new Cylinder4();
+        System.out.println("Cylinder:"
+                + "radius:"+c.getRadius()
+                +"height"+c.getHeight()
+                +"Area:"+c.getArea()
+                +"Volume:"+c.getVolume());
+
+    }
+}
