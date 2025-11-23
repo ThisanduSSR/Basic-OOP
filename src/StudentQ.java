133f4689d550d58db644d927379e2951cf4ca1e0
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-23 23:37:53 +0530

    oop practice project

--- src/StudentQ.java
+++ src/StudentQ.java
@@ -1,2 +1,11 @@
-public class StudentQ {
+public class StudentQ extends PersonQ{
+    private int idNo;
+    private int fee;
+    private int grade;
+
+    public StudentQ(String myName,int myAge, String myGender, int idNo) {
+        super(myName,myAge,myGender);
+        this.idNo = idNo;
+    }
+
 }
