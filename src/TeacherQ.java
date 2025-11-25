f2d4cd765dd4e4ff6218a09c2a0ef8245a5f2cc4
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-25 08:09:10 +0530

    oop practice project

--- src/TeacherQ.java
+++ src/TeacherQ.java
@@ -2,8 +2,8 @@ public class TeacherQ extends PersonQ{
     private int salary;
     private String subject;
 
-    public TeacherQ(String myName, int myAge, String subject, int salary) {
-        super(myName,myAge);
+    public TeacherQ(String myName, int myAge, String subject, int salary, String myGender) {
+        super(myName,myAge,myGender);
         this.subject = subject;
         this.salary = salary;
     }
@@ -18,5 +18,9 @@ public class TeacherQ extends PersonQ{
     }
     public void setSubject(String subject){
         this.subject = subject;
+    }
+    @Override
+    public String toString() {
+        return super.toString()+", subject:"+ subject+", salary:"+salary;
     }
 }
